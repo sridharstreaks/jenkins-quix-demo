acd2a6f059e0a02024593930bd4a5bc5efeec53e
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-11-03 06:52:42 +0000

    Created producer

---
+++ send_to_kafka/main.py
@@ -0,0 +1,41 @@
+import requests
+import time
+import json
+import logging
+from quixstreams import Application
+
+
+def get_weather():
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast",
+        params={
+            "latitude": 51.5,
+            "longitude": -0.11,
+            "current": "temperature_2m",
+        },
+    )
+
+    return response.json()
+
+
+def main():
+    app = Application(
+        loglevel="DEBUG",
+    )
+
+    with app.get_producer() as producer:
+        while True:
+            weather = get_weather()
+            logging.debug("Got weather: %s", weather)
+            producer.produce(
+                topic="weather_data_demo",
+                key="London",
+                value=json.dumps(weather),
+            )
+            logging.info("Produced. Sleeping...")
+            time.sleep(30)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="DEBUG")
+    main()
